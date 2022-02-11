dda3ef50064c882aa2162039abef039b1a4a7871
MelonMania <dnwlsskfn@gmail.com>
2022-02-11 19:08:58 +0900

    Libft

--- cursus0/Libft/ft_memchr.c
+++ cursus0/Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: wolee <wolee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:24:56 by wolee             #+#    #+#             */
-/*   Updated: 2021/11/23 14:01:22 by wolee            ###   ########seoul.kr  */
+/*   Updated: 2022/02/11 18:35:36 by wolee            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	src = (unsigned char *)s;
 	find = (unsigned char)c;
-	while (i < n && src[i])
+	while (i < n)
 	{
 		if (src[i] == find)
 			return ((unsigned char *)src + i);
--- cursus0/Libft/ft_strnstr.c
+++ cursus0/Libft/ft_strnstr.c
@@ -6,27 +6,27 @@
 /*   By: wolee <wolee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:53:23 by wolee             #+#    #+#             */
-/*   Updated: 2021/11/10 13:42:03 by wolee            ###   ########seoul.kr  */
+/*   Updated: 2022/02/11 09:45:14 by wolee            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *str, const char *find, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	if (!needle[i])
-		return ((char *)haystack);
-	while (haystack[i] && i < len)
+	if (!find[i])
+		return ((char *)str);
+	while (str[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && (i + j) < len)
+		while (str[i + j] == find[j] && (i + j) < len)
 		{
-			if (needle[j + 1] == '\0')
-				return ((char *)haystack + i);
+			if (find[j + 1] == '\0')
+				return ((char *)str + i);
 			j++;
 		}
 		i++;
